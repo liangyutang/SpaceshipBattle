9c23e6fa8ac30a8fe6aa26f200cbb12a79496c98
liang yutang <957753243@qq.com>
2023-06-12 11:30:49 +0800

    敌人创建

---
+++ Source/SpaceshipBattle/Public/Enemy.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class SPACESHIPBATTLE_API AEnemy : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AEnemy();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, Category = "Component")
+	UStaticMeshComponent* ShipSM;
+
+	UPROPERTY(VisibleAnywhere, Category = "Component")
+	class USphereComponent* CollisionComponent;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
